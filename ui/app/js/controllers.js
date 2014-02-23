c3ad09d755bb8d4ad75f562b8a5d845c2c15a018
Elian ORIOU <elian.oriou@4sh.fr>
2014-02-23 22:50:34 +0100

    Add group filtering

--- ui/app/js/controllers.js
+++ ui/app/js/controllers.js
@@ -5,12 +5,18 @@
 angular.module('myApp.controllers', []).
   controller('JobsCtrl', ['$scope', '$http', 'Jobs', function($scope, $http, Jobs) {
         $scope.jobs = [];
+        $scope.groups = {};
 
         function loadJobs() {
             $http.get('/api/jobs').success(function(jobs) {
+
                 $scope.jobs = jobs;
 
-                angular.forEach($scope.jobs, function(job) {
+                _.each($scope.jobs, function(job) {
+                    $scope.groups[job.group] = true;
+                });
+
+                _.each($scope.jobs, function(job) {
                     $http.get('/api/groups/' + job.group + '/jobs/' + job.name).success(function(j) {
                         job.triggers = j.triggers;
                     });
@@ -23,12 +29,11 @@ angular.module('myApp.controllers', []).
             if (confirm('Are you sure you want to delete this job?')) {
                 $http.delete('/api/groups/' + job.group + '/jobs/' + job.name).success(function() {
                     alert('job deleted');
-
                 });
             }
         };
 
-        $scope.isTriggered = function(trigger) {
+        $scope.isDone = function(trigger) {
             return trigger.when && moment().isAfter(moment(trigger.when));
         };
 
